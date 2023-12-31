69428af1327f5f3c0128349908be9d79890664a6
Hsungjin <hsungjin142@gmail.com>
2023-12-31 15:45:26 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "framework-de157c6b30d4790e4e81.js"
   },
   {
-    "url": "app-482394c8a4dce3749c7d.js"
+    "url": "app-4be34fe033f33726346d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b3c26c61425091039f88e359e503e409"
+    "revision": "2e6fce5f71c55959fcb30cbc8d6a6df4"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -166,7 +166,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-482394c8a4dce3749c7d.js`))) {
+  if (!resources || !(await caches.match(`/app-4be34fe033f33726346d.js`))) {
     return await fetch(event.request)
   }
 
