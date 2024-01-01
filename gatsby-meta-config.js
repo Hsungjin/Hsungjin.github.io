f125d7a2c1dda9965c0fa99749db2a31bb4b945c
Hsungjin <hsungjin142@gmail.com>
2024-01-01 17:21:23 +0900

    2023 회고 글 추가, 댓글 기능 추가

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: `Hsungjin/Hsungjin.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
   ga: 'G-EHEZ2KNV9Q', // Google Analytics Tracking ID
