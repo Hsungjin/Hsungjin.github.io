d3901bd04cbd2161139d19c3814f56832126841b
Hsungjin <hsungjin142@gmail.com>
2024-01-05 04:32:07 +0900

    Google analytics 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,11 +42,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
         trackingId: metaConfig.ga,
-        head: true,
-        anonymize: true,
+        head: true
+        // anonymize: true,
       },
     },
     {
